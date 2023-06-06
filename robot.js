c48fc37bc26e5d4af64cffc0a0e38ae5af6073a3
JP Ferreira <jpferreira1982@gmail.com>
2023-06-06 16:23:11 +1000

    The robot is now able to turn left and right This commit included some minor refactoring

--- robot.js
+++ robot.js
@@ -1,4 +1,4 @@
-const robot = document.getElementById("robot")
+const robot = document.getElementById('robot')
 
 const placeRobot = () => {
   const x = document.getElementById('x').value;
@@ -27,15 +27,15 @@ const goForward = () => {
   let locationY = robot.parentNode.dataset.value[0];
   const facingDirection = robot.dataset.direction;
 
-  const alertMessage = "The robot will fall off the table! You have to change direction";
+  const alertMessage = 'The robot will fall off the table! You have to change direction';
 
-  if (facingDirection === "east" && locationX !== "5") {
+  if (facingDirection === 'east' && locationX !== '5') {
     locationX = parseInt(locationX, 10) + 1;
-  } else if (facingDirection === "west" && locationX !== "1") {
+  } else if (facingDirection === 'west' && locationX !== '1') {
     locationX = parseInt(locationX, 10) - 1;
-  } else if (facingDirection === "north" && locationY.charCodeAt(0) !== 101) {
+  } else if (facingDirection === 'north' && locationY.charCodeAt(0) !== 101) {
     locationY = String.fromCharCode(locationY.charCodeAt(0) + 1);
-  } else if (facingDirection === "south" && locationY.charCodeAt(0) !== 97) {
+  } else if (facingDirection === 'south' && locationY.charCodeAt(0) !== 97) {
     locationY = String.fromCharCode(locationY.charCodeAt(0) - 1);
   } else {
     alert(alertMessage);
@@ -44,4 +44,28 @@ const goForward = () => {
 
   const square = document.querySelector(`div[data-value="${locationY}${locationX}"]`);
   square.appendChild(robot);
+};
+
+const turnLeft = () => {
+  const direction = robot.dataset.direction;
+  const directions = ['north', 'east', 'south', 'west'];
+  const currentIndex = directions.indexOf(direction);
+
+  if (currentIndex !== -1) {
+    const newDirection = directions[(currentIndex + 3) % 4];
+    robot.classList.replace(`face-${direction}`, `face-${newDirection}`);
+    robot.dataset.direction = newDirection;
+  }
+};
+
+const turnRight = () => {
+  const direction = robot.dataset.direction;
+  const directions = ['north', 'west', 'south', 'east'];
+  const currentIndex = directions.indexOf(direction);
+
+  if (currentIndex !== -1) {
+    const newDirection = directions[(currentIndex + 3) % 4];
+    robot.classList.replace(`face-${direction}`, `face-${newDirection}`);
+    robot.dataset.direction = newDirection;
+  }
 };
