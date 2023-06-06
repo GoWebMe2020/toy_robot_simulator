3adfdb8195391ff01eafffe4050a926c383666b6
JP Ferreira <jpferreira1982@gmail.com>
2023-06-06 15:08:13 +1000

    The robot is now able to report back its location with a click

--- robot.js
+++ robot.js
@@ -7,5 +7,19 @@ const placeRobot = () => {
   const square = document.querySelector(`div[data-value="${x}${y}"]`);
 
   robot.classList.add(`face-${direction}`);
+  robot.dataset.direction = direction
   square.appendChild(robot);
+}
+
+const reportLocation = () => {
+  const reportElement = document.getElementById('report')
+  const locationX = robot.parentNode.dataset.value[0]
+  const locationY = robot.parentNode.dataset.value[1]
+  const facingDirection = robot.dataset.direction
+
+  reportElement.innerText = `X: ${locationX.toUpperCase()}, Y: ${locationY}, Facing Direction: ${capitaliseDirection(facingDirection)}`
+}
+
+const capitaliseDirection = (directionInString) => {
+  return directionInString.charAt(0).toUpperCase() + directionInString.slice(1)
 }
